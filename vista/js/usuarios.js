b1bec2e4fa134e2f882f95dcd9c396661449ec5d
Rafael Antonio Gavidia Montaña <ragavidia@misena.edu.co>
2021-09-24 07:29:58 -0500

    cargar roles js

--- vista/js/usuarios.js
+++ vista/js/usuarios.js
@@ -1,5 +1,55 @@
 // Rafael---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-$(document).ready(function () {
+$(document).ready(function (opcion, id, principal) {
+
+    cargarRol(1)
+     
+    function cargarRol(){
+
+        var cargarRoles = "ok";
+        var objDataRoles =  new FormData();
+        objDataRoles.append("cargarRoles",cargarRoles);
+
+        
+        $.ajax({
+
+
+            url: "control/usuariosControl.php",
+            type: "post",
+            dataType: "json",
+            data: objDataRoles,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (respuesta) {
+
+                if(opcion = 1){
+
+                    $("#selectUsuario").html();
+                    
+                    function listarRoles(item, index){
+
+                        $("#selectRol").append('<option value="' + item.idRol  + '">' + item.descripcion + ' </option>')
+
+                    }
+                }else if(opcion = 2){
+
+                    $("#selectModUsuario").html();
+
+
+                }
+
+
+
+
+            }
+
+        })
+
+    }
+
+
+
+    // Registrar
 
     $("#btnRegistrarUsuario").click(function () {
 
@@ -8,7 +58,7 @@ $(document).ready(function () {
         var apellido = $("#txtApellido").val()
         var email = $("#txtEmail").val();
         var contraseña = $("#txtContraseña").val();
-        var idRol = document.getElementById("selectRol").value;
+        var idRol = document.getElementById("selectUsuario").value;
 
         var objData = new FormData();
         objData.append("nombre", nombre);
